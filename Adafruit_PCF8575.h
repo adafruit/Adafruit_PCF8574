f03dd9fb8800728679fa51e9b35c23cdd510bd89
lady ada <limor@ladyada.net>
2022-09-28 16:16:13 -0400

    add PCF8575 support

--- Adafruit_PCF8575.h
+++ Adafruit_PCF8575.h
@@ -1,9 +1,9 @@
 /*!
- *  @file Adafruit_PCF8574.h
+ *  @file Adafruit_PCF8575.h
  *
- * 	I2C Driver for PCF8574 I2C GPIO expander
+ * 	I2C Driver for PCF8575 I2C GPIO expander
  *
- * 	This is a library for the Adafruit PCF8574 breakout:
+ * 	This is a library for the Adafruit PCF8575 breakout:
  * 	http://www.adafruit.com/products
  *
  * 	Adafruit invests time and resources providing this open source code,
@@ -14,32 +14,32 @@
  *	BSD license (see license.txt)
  */
 
-#ifndef _ADAFRUIT_PCF8574_H
-#define _ADAFRUIT_PCF8574_H
+#ifndef _ADAFRUIT_PCF8575_H
+#define _ADAFRUIT_PCF8575_H
 
 #include "Arduino.h"
 #include <Adafruit_BusIO_Register.h>
 #include <Adafruit_I2CDevice.h>
 
-#define PCF8574_I2CADDR_DEFAULT 0x20 ///< PCF8574 default I2C address
+#define PCF8575_I2CADDR_DEFAULT 0x20 ///< PCF8575 default I2C address
 
 /*!
  *    @brief  Class that stores state and functions for interacting with
- *            the PCF8574 I2C Expander
+ *            the PCF8575 I2C Expander
  */
-class Adafruit_PCF8574 {
+class Adafruit_PCF8575 {
 public:
-  Adafruit_PCF8574();
-  bool begin(uint8_t i2c_addr = PCF8574_I2CADDR_DEFAULT, TwoWire *wire = &Wire);
+  Adafruit_PCF8575();
+  bool begin(uint8_t i2c_addr = PCF8575_I2CADDR_DEFAULT, TwoWire *wire = &Wire);
 
-  bool digitalWriteByte(uint8_t d);
+  bool digitalWriteWord(uint16_t d);
   bool digitalWrite(uint8_t pinnum, bool val);
-  uint8_t digitalReadByte(void);
+  uint16_t digitalReadWord(void);
   bool digitalRead(uint8_t pinnum);
   bool pinMode(uint8_t pinnum, uint8_t val);
 
 private:
-  uint8_t _readbuf = 0, _writebuf = 0;
+  uint16_t _readbuf = 0, _writebuf = 0;
 
   Adafruit_I2CDevice *i2c_dev;
 };
